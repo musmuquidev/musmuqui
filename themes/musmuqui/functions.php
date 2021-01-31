6ffbf01aa28e519f4b09767fbadc64f722128903
musmuquidev <75567072+musmuquidev@users.noreply.github.com>
2021-01-31 12:18:18 +0100

    styles folder and functions

---
+++ themes/musmuqui/functions.php
@@ -0,0 +1,24 @@
+
+<?php
+/*
+//Scripts y Styles
+function musmuqui_scripts_styles() {
+
+    //Carga de CSS:
+
+    //Cargamos normalize
+    wp_enqueue_style('normalize', get_template_directory_uri().'/css/normalize.css', array(), '8.0.1');
+
+    //Cargamos la hoja de estilos principal (style.css). En las dependencias metemos todas líneas que vamos cargando
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.0');
+
+    //Carga de SCRIPTS:
+
+    //Cargamos el script principal
+    wp_enqueue_script('js', get_template_directory_uri().'/js/js.js', array('jquery'), '1.0.0', true);
+}
+
+//este hook va a cargar todas las hojas de estilos que tengamos dentro del tema
+add_action('wp_enqueue_scripts', 'musmuqui_scripts_styles');
+*/
+ ?>
